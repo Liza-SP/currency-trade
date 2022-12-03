372fd69ad2d51eb713637a1e09633d810efb447e
Liza-SP <elizavetasolodovnik99@gmail.com>
2022-12-03 22:30:40 +0300

    make request - data in redux

--- client/src/components/TradingPage/Currency.jsx
+++ client/src/components/TradingPage/Currency.jsx
@@ -1,64 +1,33 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addRequest } from '../../redux/reducers/requestsReducer';
+import { useSelector } from 'react-redux';
 import CurrencyDropDown from './CurrencyDropDown';
 import styles from './index.module.css';
+import ModalCard from './ModalCard/ModalCard';
 
 export default function Currency() {
   const [active, setActive] = useState(false);
+  // цель заявки - купить или продать валюту
   const [side, setSide] = useState(false);
-  const dispatch = useDispatch();
+  // выбранная валюта
   const currency = useSelector((s) => s.currency);
+  // рандомно взятый курс
   const rate = useSelector((s) => s.rate);
 
-  const requestHandler = () => {
-    dispatch(addRequest({
-      side, price: '', instrument: currency, volume: '', timestamp: new Date(),
-    }));
-    setActive(!active);
-    setSide('');
-  };
   return (
     <>
       <CurrencyDropDown />
       <div className={styles.buttons}>
         <div>
-          <button type="button" className="button is-success is-light" onClick={() => { setActive(!active); setSide('BUY'); }}>BUY</button>
+          <button type="button" className="button is-success is-light oneBtn" onClick={() => { setActive(!active); setSide('buy'); }}>BUY</button>
           <div>{rate[currency]?.buy}</div>
         </div>
         <div>
-          <button type="button" className="button is-danger is-light" onClick={() => { setActive(!active); setSide('SELL'); }}>SELL</button>
+          <button type="button" className="button is-danger is-light oneBtn" onClick={() => { setActive(!active); setSide('sell'); }}>SELL</button>
           <div>{rate[currency]?.sell}</div>
         </div>
       </div>
-
-      <div className={active ? 'modal is-active' : 'modal'}>
-        <div className="modal-background" onClick={() => setActive(!active)} />
-        <div className="modal-card">
-          <header className="modal-card-head">
-            Make Order
-          </header>
-          <section className="modal-card-body">
-            <div className="card-body">
-
-              {/* <Form  /> */}
-
-            </div>
-          </section>
-          <footer className="modal-card-foot">
-            <div className="field is-grouped">
-              <div className="control">
-                <button type="button" onClick={() => { setActive(!active); setSide(''); }} className="button">Cancel</button>
-              </div>
-              <div className="control">
-                <button type="button" onClick={requestHandler} className="button is-primary">OK</button>
-              </div>
-            </div>
-          </footer>
-        </div>
-      </div>
+      {/* Модальное окно */}
+      <ModalCard active={active} side={side} setActive={setActive} setSide={setSide} />
     </>
   );
 }
--- client/src/components/TradingPage/ModalCard/ModalCard.jsx
+++ client/src/components/TradingPage/ModalCard/ModalCard.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addRequest } from '../../../redux/reducers/requestsReducer';
 
-export default function ModalCard() {
+export default function ModalCard({
+  active, setActive, setSide, side,
+}) {
+  const dispatch = useDispatch();
+  const volumeRef = useRef();
+  // выбранная валюта
+  const currency = useSelector((s) => s.currency);
+  // рандомно взятый курс
+  const rate = useSelector((s) => s.rate);
+  // добавление заявки от пользователя
+  const requestHandler = () => {
+    const currentRate = rate[currency];
+    dispatch(addRequest({
+      side, price: currentRate[side], instrument: currency, volume: volumeRef.current.value, timestamp: new Date(),
+    }));
+    // закрытие модалки
+    setActive(!active);
+    // обнуление цели заявки
+    setSide('');
+  };
   return (
-    <>
-      <div>ModalCard</div>
-      <div>info</div>
-      <div>buttons</div>
-    </>
+    <div className={active ? 'modal is-active' : 'modal'}>
+      <div className="modal-background" onClick={() => setActive(!active)} />
+      <div className="modal-card">
+        <header className="modal-card-head">
+          Make Order
+        </header>
+        <section className="modal-card-body">
+          <div className="card-body">
+            {/* пока не выбрали валюту, нельзя оставить заявку */}
+            {currency !== 'Choose currency' ? (
+              <>
+                <div className="text">{`${side.toUpperCase()} ${rate[currency][side]} ${currency}`}</div>
+                <div className="field">
+                  <label className="label">Volume</label>
+                  <div className="control">
+                    <input className="input" type="text" ref={volumeRef} />
+                  </div>
+                </div>
+              </>
+            ) : 'Choose currency first'}
+          </div>
+        </section>
+
+        <footer className="modal-card-foot">
+          <div className="field is-grouped">
+            <div className="control">
+              <button type="button" onClick={() => { setActive(!active); setSide(''); }} className="button">Cancel</button>
+            </div>
+            {currency !== 'Choose currency' && (
+              <div className="control">
+                <button type="button" onClick={requestHandler} className="button is-primary">OK</button>
+              </div>
+            )}
+          </div>
+        </footer>
+      </div>
+    </div>
   );
 }
